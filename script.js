e3ebe713ce007c4308f0b2be9c667064ad5c09cc
Dilsheen Kaur <56467165+dilsheen16@users.noreply.github.com>
2021-08-14 23:12:07 +0530

    Update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //Global variables
 let dataObj = {};//contains data in sheet
 let lastCell; //to maintain lastcell address which comes useful while de-selecting a cell
-
 //For creating 1-100 row numbers
 let rowNumberSection = document.querySelector(".row-number-section");
 for(let i=1;i<=100;i++){
@@ -29,54 +28,6 @@ cellSection.addEventListener("scroll",function(e){
     rowNumberSection.style.transform = `translateY(-${e.currentTarget.scrollTop}px)`;
 })
 
-//formula bar
-let formulaInput = document.querySelector(".formula-input");
-formulaInput.addEventListener("keydown",function(e){
-    if(e.key=="Enter"){
-        console.log("Evaluating");
-        let typedFormula = e.currentTarget.value;
-
-        if(!lastCell) return;
-         
-        let selectedCellAddres = lastCell.getAttribute("data-address");
-        let cellObj = dataObj[selectedCellAddres];
-        cellObj.formula = typedFormula;
-
-        //to remove old values from downstream
-        let upstream = cellObj.upstream;
-        for(let k=0;k<upstream.length;k++){
-            removeFromDownstream(upstream[k],selectedCellAddres);
-        }
-        cellObj.upstream = [];
-
-        //to update upstream
-        let splitFormula = typedFormula.split(" ");
-        let cellsInFormula=[];
-        for(let i=0;i<splitFormula.length;i++){
-            if((splitFormula[i]!="+" && "-" && "*" && "/" && " ") &&  isNaN(splitFormula[i])){
-                cellsInFormula.push(splitFormula[i]);
-            }
-        }
-        cellObj.upstream = cellsInFormula;
-
-        //to update downstream of parent cells(cells containg currentCell in their downstream)
-        for(let i=0;i<cellsInFormula.length;i++){
-            addToDownstream(cellsInFormula[i],selectedCellAddres);
-        }
-        
-
-        calculateValueOfCell(cellsInFormula,selectedCellAddres,typedFormula);
- 
-        let downstream = cellObj.downstream;
-        for(let i=0;i<downstream.length;i++){
-            updateCell(downstream[i]);
-        }
-        dataObj[selectedCellAddres] = cellObj;
-        console.log(dataObj);
-    } 
-})
-
-
 //grid cells
 //nested loop like m*n
 //for rows
@@ -93,6 +44,7 @@ for(let i=1;i<=100;i++){
         let cellDiv = document.createElement("div");
         cellDiv.contentEditable = true;//so we can write in cell
         //OR cellDiv.setAttribute("contentEditable",true);
+        cellDiv.spellcheck = false;//to remove spelling check
         cellDiv.classList.add("cell");
         cellDiv.setAttribute("data-address",cellAddress);
         
@@ -103,6 +55,14 @@ for(let i=1;i<=100;i++){
                 formula:undefined,
                 downstream:[],
                 upstream:[],
+                align: "left",
+                color: "black",
+                bgColor: "white",
+                fontFamily: "Montserrat",
+                fontSize: "15px",
+                fontWeight: "normal",//for bold
+                fontStyle: "normal",//for italic
+                textDecoration: "none",//for underline
         };
 
         cellDiv.addEventListener("input",function(e){
@@ -147,15 +107,111 @@ for(let i=1;i<=100;i++){
             let selectedCellAddres = document.querySelector(".selected-cell-div");
             let currCellAddress = e.currentTarget.getAttribute("data-address")
             selectedCellAddres.innerText = currCellAddress;
+
+            //to show formula in formula bar if it exists
+            let formulaBar = document.querySelector(".formula-input");
+            let currCellFormula = dataObj[currCellAddress].formula;
+            if(currCellFormula!=undefined){
+                formulaBar.value = currCellFormula;
+            }
+            else{
+                formulaBar.value = "";
+            }
         });
 
+        // cellDiv.addEventListener("click",function(e){
+        //     let currCell = e.currentTarget;
+        //     let currCellObj = dataObj[currCell];
+        //     if(currCell.innerText==""){
+        //     }
+        // })
         rowDiv.append(cellDiv);
 
     }
     cellSection.append(rowDiv);
 }
 
+//to get data from local storge
+if(localStorage.getItem("sheet")){
+    dataObj =  JSON.parse(localStorage.getItem("sheet"));
 
+    for(let x in dataObj){
+        let cell = document.querySelector(`[data-address='${x}']`);
+        if(dataObj[x].value){
+            cell.innerText = dataObj[x].value;
+            cell.style.textAlign = dataObj[x].align;
+            cell.style.backgroundColor = dataObj[x].bgColor;
+            cell.style.color = dataObj[x].color;
+            cell.style.fontFamily = dataObj[x].fontFamily;
+            cell.style.fontSize = dataObj[x].fontSize;
+            cell.style.fontWeight = dataObj[x].fontWeight;
+            cell.style.fontStyle = dataObj[x].fontStyle;
+            cell.style.textDecoration = dataObj[x].textDecoration;
+        }
+
+    }
+}
+
+//formula bar
+let formulaInput = document.querySelector(".formula-input");
+formulaInput.addEventListener("keydown",function(e){
+    if(e.key=="Enter"){
+        let typedFormula = e.currentTarget.value;
+
+        if(!lastCell) return;
+         
+        let selectedCellAddres = lastCell.getAttribute("data-address");
+        let cellObj = dataObj[selectedCellAddres];
+        cellObj.formula = typedFormula;
+
+        //to remove old values from downstream
+        let upstream = cellObj.upstream;
+        for(let k=0;k<upstream.length;k++){
+            removeFromDownstream(upstream[k],selectedCellAddres);
+        }
+        cellObj.upstream = [];
+
+        //to update upstream
+        let splitFormula = typedFormula.split(" ");
+        let cellsInFormula=[];
+        for(let i=0;i<splitFormula.length;i++){
+            if(splitFormula[i]!="+" && splitFormula[i]!="-" && splitFormula[i]!="*" && splitFormula[i]!="/" && splitFormula[i]!=" "  &&  isNaN(splitFormula[i])){
+                cellsInFormula.push(splitFormula[i]);
+            }
+        }
+        cellObj.upstream = cellsInFormula;
+
+        //to update downstream of parent cells(cells containg currentCell in their downstream)
+        for(let i=0;i<cellsInFormula.length;i++){
+            addToDownstream(cellsInFormula[i],selectedCellAddres);
+        }
+        
+
+        calculateValueOfCell(cellsInFormula,selectedCellAddres,typedFormula);
+ 
+        let downstream = cellObj.downstream;
+        for(let i=0;i<downstream.length;i++){
+            updateCell(downstream[i]);
+        }
+        dataObj[selectedCellAddres] = cellObj;
+        console.log(dataObj);
+
+        formulaInput.value = "";//to empty formula bar
+    } 
+})
+
+
+// dataObj["A1"].value = 20;
+// dataObj["A1"].downstream = ["B1"];
+// dataObj["B1"].formula = "2 * A1";
+// dataObj["B1"].upstream = ["A1"];
+// dataObj["B1"].value = 40;
+
+// let a1Cell = document.querySelector("[data-address='A1']");
+// let b1Cell = document.querySelector("[data-address='B1']");
+
+// a1Cell.innerText = 20;
+// b1Cell.innerText = 40;
 //if formula is being changed to a value or formula is being changed
 //C1: Formula(2*A1);
 //A1 : parent
@@ -223,6 +279,7 @@ function addToDownstream(parent,child){
     dataObj[parent].downstream.push(child); 
 } 
 
+//to calculate value of typed formula
 function calculateValueOfCell(cellsInFormula,cell,typedformula){
     let valObj = {};
     for(let i=0;i<cellsInFormula.length;i++){
@@ -241,4 +298,54 @@ function calculateValueOfCell(cellsInFormula,cell,typedformula){
 }
 
  
+//file options
+let file = document.querySelector(".fileOptions");
+file.addEventListener("click",function(){
+    let fileList = document.querySelector(".fileList");
+    if(fileList.style.display=="none" || fileList.style.display==""){
+        fileList.style.display="flex";
+        let fileListOptions = document.querySelectorAll(".fileList div");
+        fileListOptions[0].addEventListener("click",function(){
+            // fileListOptions[0].style.backgroundColor = "#2ecc71";
+            localStorage.setItem("sheet",JSON.stringify(dataObj));
+        })
+        fileListOptions[1].addEventListener("click",function(){
+            //to empty on UI
+            for(x in dataObj){
+                let cell = document.querySelector(`[data-address='${x}']`);
+                cell.innerText = "";
+                cell.style.backgroundColor = "white";
+                dataObj[x].value = undefined;
+                dataObj[x].formula = undefined;
+                dataObj[x].downstream = [];
+                dataObj[x].upstream = [];
+                dataObj[x].align = "left";
+                dataObj[x].color = "black";
+                dataObj[x].bgColor = "white";
+                dataObj[x].fontFamily = "Montserrat";
+                dataObj[x].fontSize = "15px";
+                dataObj[x].fontWeight = "normal";
+                dataObj[x].fontStyle = "normal";
+                dataObj[x].textDecoration = "none";
+                dataObj[x].fontSize = "15px";
+            }
+            //to remove from local storage
+            localStorage.setItem("sheet","");
+        })
+    } 
+    else{
+        fileList.style.display="none";
+    }
+
+})
+
+
+//help option
+let help = document.querySelector(".helpOption");
+let modal = document.querySelector(".modal");
+help.addEventListener("click",function() {
+    let modal = document.createElement("div");
+    modal.classList.add("modal");
+    body.append(modal);
+  })
 
